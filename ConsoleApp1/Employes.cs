5418465fa9946fea9d4e351b85c1a5253f7224f2
Andrew Ahverdov <wm-andrew@mail.ru>
2022-02-27 10:42:45 +0300

    First struct

---
+++ ConsoleApp1/Employes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldConsoleApp
+{
+    struct Employes
+    {
+        //ID
+        public string ID { get; set; }
+        //Дата и время добавления записи
+        public string DateAndTime { get; set; }
+        //Ф.И.О.
+        public string Name { get; set; }
+        //Возраст
+        public string Age { get; set; }
+        //Рост
+        public string Hight { get; set; }
+        //Дата рождения
+        public string DayOfBurth { get; set; }
+        //Место рождения
+        public string PlaceOfBurn { get; set; }
+
+        /// <summary>
+        /// Конструктор с подстановкой текущей даты
+        /// </summary>               
+        /// <param name="name"></param>
+        /// <param name="age"></param>
+        /// <param name="hight"></param>
+        /// <param name="dayOfBurth"></param>
+        /// <param name="placeOfBurn"></param>
+        public Employes(string name, string age, string hight, string dayOfBurth, string placeOfBurn)
+        {
+            this.ID = Guid.NewGuid().ToString();
+            this.DateAndTime = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
+            this.Name = name;
+            this.Age = age;
+            this.Hight = hight;
+            this.DayOfBurth = dayOfBurth;
+            this.PlaceOfBurn = placeOfBurn;
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,44 +10,126 @@ namespace HelloWorldConsoleApp
     {
         static void Main(string[] args)
         {
-            // Задание 1
+            // Требуется создать справочник сотрудники. В файле хранится: 
+            //ID
+            //Дата и время добавления записи
+            //Ф.И.О.
+            //Возраст
+            //Рост
+            //Дата рождения
+            //Место рождения            
+            
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Задание №1");
+            Console.WriteLine("Справочник \"Сотрудники\"");
 
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("\nВведите какое-нибудь длинное предложение, содержащее пробелы:");
-            Console.ForegroundColor = ConsoleColor.White;
+            string fileName = "db.txt";
+            char key = 'д';
 
-            var userStringMas = SplitStringToMas(Console.ReadLine());
-
-            WriteLineMas(userStringMas);
-
-            Console.ReadKey();
-        }
-
-        /// <summary>
-        /// Возвращает массив слов из введенной строки
-        /// </summary>
-        /// <param name="Строка">userString</param>
-        /// <returns></returns>
-        static string[] SplitStringToMas(string userString)
-        {
-            string[] mas = userString.Split(' ');
-            return mas;
-        }
-
-        /// <summary>
-        /// Выводит на экран массив
-        /// </summary>
-        /// <param name="Массив">mas</param>
-        static void WriteLineMas(string[] mas)
-        {
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine("\nПолучившийся массив слов:");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            foreach (var word in mas)
+            do
             {
-                Console.WriteLine(word);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("\nВведите одну из следущих команд:");
+                Console.WriteLine("1 - Вывести данные на экран");
+                Console.WriteLine("2 - Заполнить данные и добавить новую запись в конец файла");
+                Console.Write("Введите команду: ");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        if (File.Exists(fileName))
+                        {
+                            ReadFileAndShow(fileName);
+                        }
+                        else
+                        {                            
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Не найден файл \"db.txt\", сначала создайте хотябы одну запись!");                            
+                        }
+                        break;
+                    case "2":
+                        AddData(fileName);
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Не верно введена команда! ");
+                        break;
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("\nПродолжить работу со справочником? н/д");
+                key = Console.ReadKey(true).KeyChar;
+                Console.WriteLine();
+
+            } while (char.ToLower(key) == 'д');
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("\nСпасибо за использование справочника!");
+            Thread.Sleep(3000);
+        }
+        /// <summary>
+        /// Добавить данные в файл "db.txt"
+        /// </summary>
+        static void AddData(string fileName)
+        {
+            using (StreamWriter sw = new StreamWriter(fileName, true, Encoding.Unicode))
+            {
+                char keyAddData = 'д';                
+                do
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"\n{"Добавление новой заметки",53}");
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    Employes employes = new Employes();
+
+                    employes.ID = Guid.NewGuid().ToString();
+                    Console.WriteLine($"{"ID: ",29}{employes.ID}");
+ 
+                    employes.DateAndTime = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
+                    Console.WriteLine($"{"Дата и время заметки: ",29}{employes.DateAndTime}");
+
+                    Console.Write($"{"ФИО: ",29}");                    
+                    employes.Name = Console.ReadLine();
+
+                    Console.Write($"{"Введите возраст: ",29}");                   
+                    employes.Age = Console.ReadLine();
+
+                    Console.Write($"{"Введите рост: ",29}");                    
+                    employes.Hight = Console.ReadLine();
+
+                    Console.Write($"{"Дата рождения: ",29}");                    
+                    employes.DayOfBurth = Console.ReadLine();
+
+                    Console.Write($"{"Место рождения: ",29}");                    
+                    employes.PlaceOfBurn = Console.ReadLine();
+
+                    sw.WriteLine($"{employes.ID}#{employes.DateAndTime}#{employes.Name}#{employes.Age}#{employes.Hight}#{employes.DayOfBurth}#{employes.PlaceOfBurn}#");
+                    sw.Close();                    
+                    
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"{"Заметка создана!",45}");                   
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("\nДобавить еще данные? н/д");
+                    keyAddData = Console.ReadKey(true).KeyChar;
+                } while (char.ToLower(keyAddData) == 'д');
+            }
+        }
+        /// <summary>
+        /// Считать данные и вывести на экран
+        /// </summary>
+        static void ReadFileAndShow(string fileName)
+        {
+            using (StreamReader sr = new StreamReader(fileName, Encoding.Unicode))
+            {
+                string line;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"\n{"ID",3}{"Дата и время",17}{"ФИО автора",40}{"Возраст",8}{"Рост",5}{"Дата рождения",14}{"Место рождения",20}");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] data = line.Split('#');
+                    Console.WriteLine($"{data[0],3}{data[1],17}{data[2],40}{data[3],8}{data[4],5}{data[5],14}{data[6],20}");
+                }
             }
         }
     }
