d774b858a3643118624a73db9377b32b1d1b084e
Andrew Ahverdov <wm-andrew@mail.ru>
2022-03-03 16:05:09 +0300

    Structures

--- ConsoleApp1/Employes.cs
+++ ConsoleApp1/Employes.cs
@@ -6,40 +6,134 @@ using System.Threading.Tasks;
 
 namespace HelloWorldConsoleApp
 {
+    /// <summary>
+    /// Структура "Сотрудники"
+    /// </summary>
     struct Employes
     {
-        //ID
-        public string ID { get; set; }
-        //Дата и время добавления записи
-        public string DateAndTime { get; set; }
-        //Ф.И.О.
-        public string Name { get; set; }
-        //Возраст
-        public string Age { get; set; }
-        //Рост
-        public string Hight { get; set; }
-        //Дата рождения
-        public string DayOfBurth { get; set; }
-        //Место рождения
-        public string PlaceOfBurn { get; set; }
+        #region Поля
+        /// <summary>
+        /// Поле "ID"
+        /// </summary>
+        private string iD;
 
         /// <summary>
-        /// Конструктор с подстановкой текущей даты
-        /// </summary>               
-        /// <param name="name"></param>
-        /// <param name="age"></param>
-        /// <param name="hight"></param>
-        /// <param name="dayOfBurth"></param>
-        /// <param name="placeOfBurn"></param>
-        public Employes(string name, string age, string hight, string dayOfBurth, string placeOfBurn)
+        /// Поле "Дата и время заметки"
+        /// </summary>
+        private string dateAndTime;
+
+        /// <summary>
+        /// Поле "ФИО"
+        /// </summary>
+        private string name;
+
+        /// <summary>
+        /// Поле "Возраст"
+        /// </summary>
+        private string age;
+
+        /// <summary>
+        /// Поле "Рост"
+        /// </summary>
+        private string hight;
+
+        /// <summary>
+        /// Поле "Дата рождения"
+        /// </summary>
+        private string dayOfBurth;
+
+        /// <summary>
+        /// Поле "Место рождения"
+        /// </summary>
+        private string placeOfBurn;
+        #endregion
+
+        #region Свойства        
+        /// <summary>
+        /// ID
+        /// </summary>
+        public string ID { get { return this.iD; } set { this.iD = value; } }
+
+        /// <summary>
+        /// Дата и время заметки
+        /// </summary>
+        public string DateAndTime { get { return this.dateAndTime; } set { this.dateAndTime = value; } }
+
+        /// <summary>
+        /// ФИО
+        /// </summary>
+        public string Name { get { return this.name; } set { this.name = value; } }
+
+        /// <summary>
+        /// Возраст
+        /// </summary>
+        public string Age { get { return this.age; } set { this.age = value; } }
+
+        /// <summary>
+        /// Рост
+        /// </summary>
+        public string Hight { get { return this.hight; } set { this.hight = value; } }
+
+        /// <summary>
+        /// Дата рождения
+        /// </summary>
+        public string DayOfBurth { get { return this.dayOfBurth; } set { this.dayOfBurth = value; } }
+
+        /// <summary>
+        /// Место рождения
+        /// </summary>
+        public string PlaceOfBurn { get { return this.placeOfBurn; } set { this.placeOfBurn = value; } }
+        #endregion
+
+        #region Конструкторы
+        /// <summary>
+        /// Конструктор со всеми параметрами
+        /// </summary> 
+        /// <param name="ID">ID</param>
+        /// <param name="DateAndTime">Дата и время</param>
+        /// <param name="Name">ФИО</param>
+        /// <param name="Age">Возраст</param>
+        /// <param name="Hight">Рост</param>
+        /// <param name="DayOfBurth">Дата рождения</param>
+        /// <param name="PlaceOfBurn">Место рождения</param>
+        public Employes(string ID, string DateAndTime, string Name, string Age, string Hight, string DayOfBurth, string PlaceOfBurn)
         {
-            this.ID = Guid.NewGuid().ToString();
-            this.DateAndTime = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
-            this.Name = name;
-            this.Age = age;
-            this.Hight = hight;
-            this.DayOfBurth = dayOfBurth;
-            this.PlaceOfBurn = placeOfBurn;
+            this.iD = ID;
+            this.dateAndTime = DateAndTime;
+            this.name = Name;
+            this.age = Age;
+            this.hight = Hight;
+            this.dayOfBurth = DayOfBurth;
+            this.placeOfBurn = PlaceOfBurn;
         }
+
+        /// <summary>
+        /// Конструктор с подстановкой текущей даты/времени и ID
+        /// </summary>          
+        /// <param name="Name">ФИО</param>
+        /// <param name="Age">Возраст</param>
+        /// <param name="Hight">Рост</param>
+        /// <param name="DayOfBurth">Дата рождения</param>
+        /// <param name="PlaceOfBurn">Место рождения</param>
+        public Employes(string Name, string Age, string Hight, string DayOfBurth, string PlaceOfBurn):
+            this("", "", Name, Age, Hight, DayOfBurth, PlaceOfBurn)
+        {
+            string stringGuid = Guid.NewGuid().ToString();
+            this.iD = stringGuid.Substring(0, 8);
+            //this.dateAndTime = $"{DateTime.Now.ToShortDateString()} {DateTime.Now.ToShortTimeString()}";            
+            this.dateAndTime = $"{DateTime.Now:g}";
+        }
+        #endregion
+
+        #region Методы
+        /// <summary>
+        /// Вывод информации в консоль
+        /// </summary>
+        /// <returns>Строка с информацией о сотруднике</returns>
+        public string Print()
+        {           
+            return $"{this.ID,9}{this.DateAndTime,17}{this.Name,35}{this.Age,8}{this.Hight,5}{this.DayOfBurth,14}{this.PlaceOfBurn,20}";
+        }
+        #endregion
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,50 +10,57 @@ namespace HelloWorldConsoleApp
     {
         static void Main(string[] args)
         {
-            // Требуется создать справочник сотрудники. В файле хранится: 
-            //ID
-            //Дата и время добавления записи
-            //Ф.И.О.
-            //Возраст
-            //Рост
-            //Дата рождения
-            //Место рождения            
-            
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Справочник \"Сотрудники\"");
 
-            string fileName = "db.txt";
+            string fileName = @"db.txt";
+            string[] header = new string[] { "Номер", "ID", "Дата и время", "ФИО", "Возраст", "Рост", "Дата рождения", "Место рождения" };
+            Repository rep = new Repository(fileName, header);
+
             char key = 'д';
 
             do
-            {
-                Console.ForegroundColor = ConsoleColor.White;
+            {                
+                Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("\nВведите одну из следущих команд:");
                 Console.WriteLine("1 - Вывести данные на экран");
                 Console.WriteLine("2 - Заполнить данные и добавить новую запись в конец файла");
+                Console.WriteLine("3 - Удалить данные по номеру строки");
+                Console.WriteLine("4 - Изменить данные по номеру строки");
+                Console.WriteLine("5 - Найти записи за определенный период");
+                Console.WriteLine("6 - Отсортировать записи по дате добавления");
+                Console.WriteLine("0 - Выход");
+                Console.ForegroundColor = ConsoleColor.White;
                 Console.Write("Введите команду: ");
 
                 switch (Console.ReadLine())
                 {
                     case "1":
-                        if (File.Exists(fileName))
-                        {
-                            ReadFileAndShow(fileName);
-                        }
-                        else
-                        {                            
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("Не найден файл \"db.txt\", сначала создайте хотябы одну запись!");                            
-                        }
+                        rep.PrintDbToConsole();
                         break;
                     case "2":
-                        AddData(fileName);
+                        rep.AddFromConsole();
+                        break;
+                    case "3":
+                        rep.Delete();
+                        break;
+                    case "4":
+                        rep.Change();
+                        break;
+                    case "5":
+                        rep.FindDate();
+                        break;
+                    case "6":
+                        rep.SortByDate();
+                        break;
+                    case "0":
                         break;
                     default:
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Не верно введена команда! ");
                         break;
                 }
+
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.Write("\nПродолжить работу со справочником? н/д");
                 key = Console.ReadKey(true).KeyChar;
@@ -64,73 +71,6 @@ namespace HelloWorldConsoleApp
             Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("\nСпасибо за использование справочника!");
             Thread.Sleep(3000);
-        }
-        /// <summary>
-        /// Добавить данные в файл "db.txt"
-        /// </summary>
-        static void AddData(string fileName)
-        {
-            using (StreamWriter sw = new StreamWriter(fileName, true, Encoding.Unicode))
-            {
-                char keyAddData = 'д';                
-                do
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($"\n{"Добавление новой заметки",53}");
-                    Console.ForegroundColor = ConsoleColor.White;
-
-                    Employes employes = new Employes();
-
-                    employes.ID = Guid.NewGuid().ToString();
-                    Console.WriteLine($"{"ID: ",29}{employes.ID}");
- 
-                    employes.DateAndTime = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
-                    Console.WriteLine($"{"Дата и время заметки: ",29}{employes.DateAndTime}");
-
-                    Console.Write($"{"ФИО: ",29}");                    
-                    employes.Name = Console.ReadLine();
-
-                    Console.Write($"{"Введите возраст: ",29}");                   
-                    employes.Age = Console.ReadLine();
-
-                    Console.Write($"{"Введите рост: ",29}");                    
-                    employes.Hight = Console.ReadLine();
-
-                    Console.Write($"{"Дата рождения: ",29}");                    
-                    employes.DayOfBurth = Console.ReadLine();
-
-                    Console.Write($"{"Место рождения: ",29}");                    
-                    employes.PlaceOfBurn = Console.ReadLine();
-
-                    sw.WriteLine($"{employes.ID}#{employes.DateAndTime}#{employes.Name}#{employes.Age}#{employes.Hight}#{employes.DayOfBurth}#{employes.PlaceOfBurn}#");
-                    sw.Close();                    
-                    
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($"{"Заметка создана!",45}");                   
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.Write("\nДобавить еще данные? н/д");
-                    keyAddData = Console.ReadKey(true).KeyChar;
-                } while (char.ToLower(keyAddData) == 'д');
-            }
-        }
-        /// <summary>
-        /// Считать данные и вывести на экран
-        /// </summary>
-        static void ReadFileAndShow(string fileName)
-        {
-            using (StreamReader sr = new StreamReader(fileName, Encoding.Unicode))
-            {
-                string line;
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"\n{"ID",3}{"Дата и время",17}{"ФИО автора",40}{"Возраст",8}{"Рост",5}{"Дата рождения",14}{"Место рождения",20}");
-                Console.ForegroundColor = ConsoleColor.White;
-
-                while ((line = sr.ReadLine()) != null)
-                {
-                    string[] data = line.Split('#');
-                    Console.WriteLine($"{data[0],3}{data[1],17}{data[2],40}{data[3],8}{data[4],5}{data[5],14}{data[6],20}");
-                }
-            }
         }
     }
 }
